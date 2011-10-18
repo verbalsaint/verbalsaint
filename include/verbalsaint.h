61cfa034fce12ce21cede0111c5d90b332341c7a
shuohuan chang <jsc9530@gmail.com>
2011-10-18 16:15:21 -0400

    Init

---
+++ include/verbalsaint.h
@@ -0,0 +1,17 @@
+#ifndef VERBALSAINT_H
+#define VERBALSAINT_H
+
+#define VERBALSAINTBEGIN namespace VERBALSAINT{
+#define VERBALSAINTEND  }
+#define VsEXCEPTIONBEGIN namespace VSEXCEPTION{
+#define VsEXCEPTIONEND  }
+#define TBBBEGIN namespace TBB{
+#define TBBEND  }
+#define VsMATRIXBEGIN namespace VSMATRIX{
+#define VsMATRIXEND }
+#define VsARGBEGIN namespace VSARG{
+#define VsARGEND    }
+#define VsPCAPBEGIN namespace VSPCAP{
+#define VsPCAPEND   }
+
+#endif // VERBALSAINT_H
